c479b9d67ebecc55bc7e60fb331748e78b8a8cd0
@MikeRzhevsky <39048059+MikeRzhevsky@users.noreply.github.com>
2019-04-30 11:11:37 +0200

    Update NumberSequenceTutorial.sql

--- NumberSequenceTutorial.sql
+++ NumberSequenceTutorial.sql
@@ -1,8 +1,9 @@
 --Tutorial number sequence for kontragent
 --https://viccherubini.com/2017/02/25/generating-custom-sequences-in-postgres/
 --we’ll need a table to track sequence names, the kontragent they are associated with, a prefix, and their next value.
---Step 1
-CREATE TABLE tbl_number_sequence (
+
+--Step 1 : 
+CREATE TABLE number_sequence (
 
   sequence_id serial NOT NULL,      -- id of records
 
@@ -21,7 +22,8 @@ CREATE TABLE tbl_number_sequence (
 ) WITH (OIDS=FALSE);
 
 -- we’ll write a short stored procedure named generate_sequence() to generate the next value in a sequence.
---Step2 
+
+--Step2 :
 CREATE OR REPLACE FUNCTION generate_sequence(_tablename text, --real record from table
 											 _sequence text) RETURNS TEXT AS $$
 
@@ -58,7 +60,7 @@ _next_value:=lpad(_next_value, _zero_pad, '0'  );
   _value := _prefix || _next_value;
 
 
-  UPDATE tbl_number_sequence SET
+  UPDATE number_sequence SET
 
     next_value = next_value + 1
 
@@ -75,11 +77,12 @@ END;
 $$ LANGUAGE plpgsql;
 
 --create first record in tbl_number_sequence
---Step3
-1	tbl_kontragent	kontragent	КЛН-	104	5
+
+--Step3:
+1	account	account	КЛН-	104	5
 
 --validate number sequence: 345 - record from tbl_kontragent, invoice from  sequence 
 --Step4 
-SELECT generate_sequence('tbl_kontragent', 'kontragent');
+SELECT generate_sequence('account', 'account');
 
 
